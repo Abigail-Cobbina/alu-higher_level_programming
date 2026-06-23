6a3c6bb0a1e7b9b6d11f926ad27895401c7af661
Abigail-Cobbina <a.amaopooco@alustudent.com>
2026-06-23 04:19:15 -0700

    Create a new sentence

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-str1 = "object-oriented programming with Python"
-str2 = "is fun and cool!"
-str3 = "is very easy"
-print(str1[:26] + str1[26:])
+str = "Python is an interpreted, interactive, object-oriented programming\
+ language that combines remarkable power with very clear syntax"
+print(str[39:66] + str[106:111] + str[0:6])
