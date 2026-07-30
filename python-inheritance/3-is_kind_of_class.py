6384cd9ad8bb6626ac15af320982b09431f2208c
Abigail-Cobbina <a.amaopooco@alustudent.com>
2026-07-30 13:36:57 -0700

    task 3

--- python-inheritance/3-is_kind_of_class.py
+++ python-inheritance/3-is_kind_of_class.py
@@ -1,6 +1,7 @@
-"""Module documentation."""
-
 #!/usr/bin/python3
+"""This module checks if an object belongs to a class or its subclasses."""
+
+
 def is_kind_of_class(obj, a_class):
-    """Return True if obj is an instance of or inherits from a_class."""
+    """Return True if obj is an instance of a_class or its subclass."""
     return isinstance(obj, a_class)
