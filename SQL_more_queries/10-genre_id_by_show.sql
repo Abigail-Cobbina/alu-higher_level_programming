6a6ebf913fec1831f8307761e95aa8a56e9eafa7
Abigail-Cobbina <a.amaopooco@alustudent.com>
2026-07-31 09:57:50 -0700

    Complete SQL more queries project

--- SQL_more_queries/10-genre_id_by_show.sql
+++ SQL_more_queries/10-genre_id_by_show.sql
@@ -1,7 +1,7 @@
--- Lists shows with linked genre IDs
+-- Lists all shows with at least one genre linked
 
 SELECT tv_shows.title, tv_show_genres.genre_id
 FROM tv_shows
 JOIN tv_show_genres
-ON tv_shows.id = tv_show_genres.tv_show_id
+ON tv_shows.id = tv_show_genres.show_id
 ORDER BY tv_shows.title, tv_show_genres.genre_id;
